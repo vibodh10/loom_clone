15ee5b4d7163d4833130e33be4958402081667a5
vibodh10 <ghimab@gmail.com>
2025-08-06 14:34:34 +0100

    Fix build error (2nd time)

--- middleware.ts
+++ middleware.ts
@@ -30,4 +30,5 @@ export default createMiddleware(validate);
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)"],
+    runtime: "nodejs",
 };
